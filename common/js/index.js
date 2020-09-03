da654a42a061c630e173a8a46c2cb3131dfbc1f7
Nashdodti <41152814+Nashdodti@users.noreply.github.com>
2020-09-03 12:58:13 +0530

    c

--- common/js/index.js
+++ common/js/index.js
@@ -7,14 +7,14 @@
  * (c) baqemono.inc. all rights reserved.
  * ---------------------------------------------
  */
-(function($){
+(function ($) {
 
 	var $images = $('#images li');
 	var $title = $('#site-name');
 
 	var timer = subTimer = null;
 	var numImages = $images.length
-	var current = Math.floor(Math.random()*numImages);
+	var current = Math.floor(Math.random() * numImages);
 	var INTERVAL = 5000;
 
 
@@ -23,14 +23,14 @@
 		12, // 1 M
 		12, // 2 I
 		14, // 3 K
-		 2, // 4 I
-		 8, // 5 Y
-		 4, // 6 A
-		 5, // 7 K
+		2, // 4 I
+		8, // 5 Y
+		4, // 6 A
+		5, // 7 K
 		15, // 8 O
 		12, // 9 B
 		16, //10 A
-		 9, //11 Y
+		9, //11 Y
 		20, //12 A
 		15, //13 S
 		17, //14 H
@@ -38,29 +38,29 @@
 	]
 
 	$title.html('');
-	$.each(title.replace(/[\t\s]/g,'').split(''),function(i,c){
-		var $s = $('<span class="c'+(i+1)+'">'+c+'</span>');
+	$.each(title.replace(/[\t\s]/g, '').split(''), function (i, c) {
+		var $s = $('<span class="c' + (i + 1) + '">' + c + '</span>');
 		var d = typo_delay[i]
-		$s.css(Modernizr.prefixedCSS('transition-delay'), (d/30)*1000+'ms')
+		$s.css(Modernizr.prefixedCSS('transition-delay'), (d / 30) * 1000 + 'ms')
 		$title.append($s);
 	});
 
 
-	$images.each(function(){
+	$images.each(function () {
 		var $img = $(this).find('img');
 		$(this).css({
-			'background-image' : 'url('+$img.attr('src')+')'
+			'background-image': 'url(' + $img.attr('src') + ')'
 		});
-		$img.css('display','none');
+		$img.css('display', 'none');
 	});
 
-	function start(){
-		timer = setTimeout(function(){
+	function start() {
+		timer = setTimeout(function () {
 			var old = current;
 			$images.eq(old).addClass('hide');
-			subTimer = setTimeout(function(){
+			subTimer = setTimeout(function () {
 				$images.eq(old).removeClass('current hide');
-			},2200)
+			}, 2200)
 			current = getRandomNext();
 			$images.eq(current).addClass('current');
 
@@ -69,16 +69,16 @@
 		}, INTERVAL);
 	}
 
-	function stop(){
+	function stop() {
 		clearTimeout(timer);
 		clearTimeout(subTimer);
 	}
 
-	function getRandomNext(){
-		var n = Math.floor(Math.random()*numImages);
-		if(n==current){
-			while(n==current){
-				n = Math.floor(Math.random()*numImages);
+	function getRandomNext() {
+		var n = Math.floor(Math.random() * numImages);
+		if (n == current) {
+			while (n == current) {
+				n = Math.floor(Math.random() * numImages);
 			}
 		}
 		return n;
@@ -87,29 +87,29 @@
 
 
 
-	function removeEvents(){
+	function removeEvents() {
 		$('#scroll-info a').off('click.index');
 		$(document).off('mousewheel.index touchmove.index');
 	}
 
 	var init_timers = []
-	$(window).one('enter.page.bq', function(){
+	$(window).one('enter.page.bq', function () {
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$title.addClass('show');
 		}, 0);
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$images.eq(current).addClass('current');
 			start();
 		}, 800);
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$('#scroll-info').addClass('show');
 		}, 1300);
 
 		init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-	}).one('exit.page.bq', function(){
+	}).one('exit.page.bq', function () {
 		removeEvents();
 		stop();
 		for (var i = init_timers.length - 1; i >= 0; i--) {
